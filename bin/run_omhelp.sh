b7fe310f0dd93b932b6b515aff7523945923755d
Brad Bell <bradbell@seanet.com>
2017-01-02 21:15:40 -0700

    Start documenting the project.
    new_file.sh: add code for *.omh files.

---
+++ bin/run_omhelp.sh
@@ -0,0 +1,41 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+#         cppad_swig: A C++ Object Library and SWIG Interface to CppAD
+#          Copyright (C) 2017-17 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#          GNU Affero General Public License version 3.0 or later see
+#                     http://www.gnu.org/licenses/agpl.txt
+# -----------------------------------------------------------------------------
+# bash function that echos and executes a command
+echo_eval() {
+	echo $*
+	eval $*
+}
+# -----------------------------------------------------------------------------
+if [ "$0" != "bin/run_omhelp.sh" ]
+then
+	echo "bin/run_omhelp.sh: must be executed from its parent directory"
+	exit 1
+fi
+# -----------------------------------------------------------------------------
+if [ "$1" != 'xml' ] && [ "$1" != 'htm' ]
+then
+	echo 'bin/run_omhelp.sh (xml|htm)'
+	exit 1
+fi
+# -----------------------------------------------------------------------------
+if [ -e doc ]
+then
+	echo_eval rm -r doc
+fi
+mkdir doc
+cd doc
+if [ "$1" == 'htm' ]
+then
+	omhelp ../cppad_swig.omh -noframe -debug
+else
+	omhelp ../cppad_swig.omh -noframe -debug -xml
+fi
+# -----------------------------------------------------------------------------
+echo 'bin/run_omhelp.sh: OK'
+exit 0
