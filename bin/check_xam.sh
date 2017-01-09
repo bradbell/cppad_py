aa3ae6c7122891876421d045b7e12ecac819e48d
Brad Bell <bradbell@seanet.com>
2017-01-09 09:19:50 -0700

    Simplify check_all files and use check_xam to make sure all up to date. .gitignore: pyc files are now in build directory so no need to ignore in source.

---
+++ bin/check_xam.sh
@@ -0,0 +1,56 @@
+#! /bin/bash -e
+# -----------------------------------------------------------------------------
+#         cppad_swig: A C++ Object Library and Swig Interface to Cppad
+#          Copyright (C) 2017-17 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#          GNU Affero General Public License version 3.0 or later see
+#                     http://www.gnu.org/licenses/agpl.txt
+# -----------------------------------------------------------------------------
+# bash function that echos and executes a command
+echo_eval() {
+	echo $*
+	eval $*
+}
+# -----------------------------------------------------------------------------
+if [ "$0" != "bin/check_xam.sh" ]
+then
+	echo "bin/check_xam.sh: must be executed from its parent directory"
+	exit 1
+fi
+# -----------------------------------------------------------------------------
+# list of xam example / test files
+xam_file_list=`ls lib/xam/*/*.xam | sed -e 's|lib/xam/||'`
+xam_fun_list=`echo $xam_file_list | sed -e 's|/|_|g' -e 's|\.xam||g'`
+# -----------------------------------------------------------------------------
+check=lib/example/CMakeLists.txt
+for file in $xam_file_list
+do
+	if ! grep $file $check > /dev/null
+	then
+		echo "check_xam.sh: $file is missing in"
+		echo $check
+		exit 1
+	fi
+done
+# -----------------------------------------------------------------------------
+lang_file_list='
+	lib/example/cplusplus/check_all.cpp
+	lib/example/octave/check_all.m.in
+	lib/example/perl/check_all.pl.in
+	lib/example/python/check_all.py.in
+'
+for lang_file in $lang_file_list
+do
+	for fun in $xam_fun_list
+	do
+		if ! grep $fun $lang_file > /dev/null
+		then
+			echo "check_xam.sh: $fun is missing in"
+			echo $lang_file
+			exit 1
+		fi
+	done
+done
+# -----------------------------------------------------------------------------
+echo 'bin/check_xam.sh: OK'
+exit 0
