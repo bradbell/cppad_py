09dc6f7dcb8358dc0a93037c1fc26dd4b8e605cc
Brad Bell <bradbell@seanet.com>
2018-07-08 12:45:15 -0700

    master: only import names in cppad_py_swig that are in python API.

--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -7,4 +7,18 @@
 # -----------------------------------------------------------------------------
 # initialize cppad_py
 # -----------------------------------------------------------------------------
-from cppad_py.cppad_py_swig import *
+# BEGIN_SORT_THIS_LINE_PLUS_1
+from cppad_py.cppad_py_swig import abort_recording
+from cppad_py.cppad_py_swig import a_double
+from cppad_py.cppad_py_swig import a_fun
+from cppad_py.cppad_py_swig import error_message
+from cppad_py.cppad_py_swig import independent
+from cppad_py.cppad_py_swig import sparse_hes_work
+from cppad_py.cppad_py_swig import sparse_jac_work
+from cppad_py.cppad_py_swig import sparse_rc
+from cppad_py.cppad_py_swig import sparse_rcv
+from cppad_py.cppad_py_swig import vec_a_double
+from cppad_py.cppad_py_swig import vec_bool
+from cppad_py.cppad_py_swig import vec_double
+from cppad_py.cppad_py_swig import vec_int
+# END_SORT_THIS_LINE_MINUS_1
