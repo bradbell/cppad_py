d55c617e6fb90bcae44b972be655d323f753e6e5
Brad Bell <bradbell@seanet.com>
2018-11-09 17:37:16 -0800

    master: __init__.py: sort entries and remove one that is not being used.

--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -8,7 +8,7 @@
 # initialize cppad_py
 # -----------------------------------------------------------------------------
 
-# Function that work as in c++
+# Function that work as is using swig interface to  c++
 # BEGIN_SORT_THIS_LINE_PLUS_1
 from cppad_py.swig import abort_recording
 from cppad_py.swig import a_double
@@ -21,31 +21,25 @@ from cppad_py.swig import vec_double
 from cppad_py.swig import vec_int
 # END_SORT_THIS_LINE_MINUS_1
 
-# functions that require a python wraper
-from cppad_py.fun             import a_fun
-from cppad_py.fun             import d_fun
-#
+
+# BEGIN_SORT_THIS_LINE_PLUS_1
+from cppad_py.fun_ctor        import d_fun_ctor
 from cppad_py.fun_forward     import a_fun_forward
 from cppad_py.fun_forward     import d_fun_forward
-#
-from cppad_py.fun_reverse     import a_fun_reverse
-from cppad_py.fun_reverse     import d_fun_reverse
-#
-from cppad_py.fun_jacobian    import a_fun_jacobian
-from cppad_py.fun_jacobian    import d_fun_jacobian
-#
 from cppad_py.fun_hessian     import a_fun_hessian
 from cppad_py.fun_hessian     import d_fun_hessian
-#
-from cppad_py.fun_ctor        import d_fun_ctor
+from cppad_py.fun             import a_fun
+from cppad_py.fun             import d_fun
+from cppad_py.fun_jacobian    import a_fun_jacobian
+from cppad_py.fun_jacobian    import d_fun_jacobian
+from cppad_py.fun_reverse     import a_fun_reverse
+from cppad_py.fun_reverse     import d_fun_reverse
 from cppad_py.hes_sparsity    import d_fun_for_hes_sparsity
 from cppad_py.hes_sparsity    import d_fun_rev_hes_sparsity
+from cppad_py.independent     import independent
 from cppad_py.sparse_hes      import d_fun_sparse_hes
 from cppad_py.sparse_jac      import d_fun_sparse_jac_for
 from cppad_py.sparse_jac      import d_fun_sparse_jac_rev
-from cppad_py.independent     import independent
 from cppad_py.sparse_rc       import sparse_rc
 from cppad_py.sparse_rcv      import sparse_rcv
-
-# utilities
-import cppad_py.utility
+# END_SORT_THIS_LINE_MINUS_1
