f76f8546ed23e71dd697da6132df7c1d496e4f9a
Brad Bell <bradbell@seanet.com>
2020-05-10 10:35:40 -0700

    master: covid_19_xam.py: refactor using more local functions.

--- example/python/numeric/covid_19_xam.py
+++ example/python/numeric/covid_19_xam.py
@@ -11,6 +11,7 @@
 #	seird
 #	Covariates
 #	Covariate
+#	cv
 # $$
 #
 # $section Example Fitting an SEIRD Model for Covid-19$$
@@ -92,8 +93,8 @@ plot_truth = False
 # $head Coefficient of Variation$$
 # This is the coefficient of variation for the differences
 # in the cumulative death data as a fraction, not a percent.
-# If this value is zero, there is a cv of zero for data simulation
-# and a cv of one in the definition of the likelihood.
+# If this value is zero, there is a CV of zero for data simulation
+# and a CV of one in the definition of the likelihood.
 # This enables checking that the parameters are identifiable with perfect data.
 # $srccode%py%
 death_data_cv = 0.1
@@ -143,6 +144,19 @@ for i in range(n_time) :
 	testing         = t / t_stop
 	covariates[i,:] = [ mobility, temperature, testing ]
 #
+# baseline_true
+baseline_true = 0.15
+#
+# cov_mul_true
+cov_mul_true  = numpy.array( [ - 0.2, - 0.3, - 0.4 ] )
+#
+# true initial conditions used to simulate data
+I0_true     = 0.02
+E0_true     = 0.02
+S0_true     = 1.0 - E0_true - I0_true
+R0_true     = 0.0
+D0_true     = 0.0
+#
 # p_fun_class
 class p_fun_class :
 	def __init__(self, beta_all) :
@@ -170,40 +184,42 @@ class p_fun_class :
 		}
 		return p
 #
+def x2seird_all(x) :
+	#
+	# unpack x
+	E0       = x[0]
+	I0       = x[1]
+	baseline = x[2]
+	cov_mul  = x[3 :]
+	#
+	# beta_all
+	beta_all = baseline * (1.0 + numpy.matmul(covariates, cov_mul))
+	#
+	# initial
+	S0 = 1.0 - E0 - I0
+	R0 = 0.0
+	D0 = 0.0
+	initial  = numpy.array( [S0, E0, I0, R0, D0] )
+	#
+	# p_fun
+	p_fun_obj = p_fun_class(beta_all)
+	p_fun     = p_fun_obj.p_fun
+	#
+	# seird_all
+	seird_all = seird_model(t_all, p_fun, initial)
+	#
+	return seird_all
+#
 # objective_d_fun
 # t_all and D_data, are constants relative in the objective function
 def objective_d_fun(t_all, D_data) :
 	#
-	# n_time, n_cov
-	n_time, n_cov = covariates.shape
-	#
-	# n_x
-	n_x = n_cov + 3
-	#
-	# x = ( E(0), I(0), baseline, cov_mul )
-	# Note that R(0) = D(0) = 0 and S(0) = 1 - E(0) - I(0)
-	x  = 0.1 * numpy.ones(n_x)
+	# x = ( E(0), I(0), baseline, m[0], m[1], m[2] )
+	x  = 0.1 * numpy.ones(6)
 	ax = cppad_py.independent(x)
 	#
-	# abeta_all
-	abaseline = ax[2]
-	acov_mul  = ax[3 :]
-	abeta_all = abaseline * (1.0 + numpy.matmul(covariates, acov_mul))
-	#
-	# ainitial
-	aE0      = ax[0]
-	aI0      = ax[1]
-	aR0      = 0.0
-	aD0      = 0.0
-	aS0      = 1.0 - aE0 - aI0
-	ainitial = numpy.array( [aS0, aE0, aI0, aR0, aD0] )
-	#
-	# p_fun
-	ap_fun_obj = p_fun_class(abeta_all)
-	ap_fun    = ap_fun_obj.p_fun
-	#
 	# compute model for data
-	aseird_all = seird_model(t_all, ap_fun, ainitial)
+	aseird_all = x2seird_all(ax)
 	#
 	# Model for the cumulative death as function of time
 	aD_model   = aseird_all[:,4] # column order is S, E, I, R, D
@@ -223,37 +239,14 @@ def objective_d_fun(t_all, D_data) :
 	objective_ad = cppad_py.d_fun(ax, aloss)
 	return objective_ad
 
-def covid_19_xam(call_count = 0) :
-	ok = True
+def simulate_data() :
 	#
-	# baseline_true
-	baseline_true = 0.15
-	#
-	# cov_mul_true
-	cov_mul_true  = numpy.array( [ - 0.2, - 0.3, - 0.4 ] )
-	#
-	# beta_all_true
-	covariate_effect = numpy.matmul(covariates, cov_mul_true)
-	beta_all_true    = baseline_true * (1.0 + covariate_effect)
-	#
-	# p_fun_true
-	p_fun_obj   = p_fun_class(beta_all_true)
-	p_fun_true  = p_fun_obj.p_fun
-	#
-	# compute model for data
-	#
-	# initial_true: (must satisfy S + E + I = 1, R = D = 0)
-	I0_true     = 0.02
-	E0_true     = 0.02
-	S0_true     = 1.0 - E0_true - I0_true
-	R0_true     = 0.0
-	D0_true     = 0.0
-	initial_true = numpy.array(
-		[ S0_true, E0_true, I0_true, R0_true, D0_true ]
-	)
+	x_true = numpy.empty(6, dtype=float)
+	x_true[0:3] = [ E0_true, I0_true, baseline_true ]
+	x_true[3 :] = cov_mul_true
 	#
 	# noiseless simulated data
-	seird_all_true = seird_model(t_all, p_fun_true, initial_true)
+	seird_all_true = x2seird_all(x_true)
 	if plot_truth :
 		ax = pyplot.subplot(111)
 		ax.plot(t_all, seird_all_true[:,0], 'b-', label='S')
@@ -265,9 +258,9 @@ def covid_19_xam(call_count = 0) :
 		pyplot.show()
 	#
 	# check conservation of masss in the compartmental model
-	sum_all_true = numpy.sum(seird_all_true, axis=1)
-	eps99 = 99.0 * numpy.finfo(float).eps
-	ok    = ok and max( abs(sum_all_true - 1.0) ) < eps99
+	# sum_all_true = numpy.sum(seird_all_true, axis=1)
+	# eps99 = 99.0 * numpy.finfo(float).eps
+	# ok    = ok and max( abs(sum_all_true - 1.0) ) < eps99
 	#
 	# actual_seed
 	if random_seed == 0 :
@@ -287,6 +280,14 @@ def covid_19_xam(call_count = 0) :
 	D_data       = numpy.cumsum(Ddiff_data)
 	D_data       = numpy.concatenate( ([0.0], D_data) )
 	#
+	return D_data
+
+def covid_19_xam(call_count = 0) :
+	ok = True
+	#
+	# D_data
+	D_data = simulate_data()
+	#
 	# objective_ad
 	objective_ad = objective_d_fun(t_all, D_data)
 	#
@@ -294,8 +295,7 @@ def covid_19_xam(call_count = 0) :
 	optimize_fun = optimize_fun_class(objective_ad)
 	#
 	# x_true
-	n_cov       = cov_mul_true.size
-	x_true      = numpy.empty( 3 + n_cov, dtype=float)
+	x_true      = numpy.empty( 6, dtype=float)
 	x_true[0:3] = [ E0_true, I0_true, baseline_true ]
 	x_true[3:]  = cov_mul_true
 	#
