578d38e83ae3704280feb0293633b66de175e6f7
Brad Bell <bradbell@seanet.com>
2020-05-14 10:14:22 -0700

    master: numeric: add the simple_inv routine.

---
+++ example/python/numeric/simple_inv.py
@@ -0,0 +1,97 @@
+# -----------------------------------------------------------------------------
+#         cppad_py: A C++ Object Library and Python Interface to Cppad
+#          Copyright (C) 2017-20 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#              GNU General Public License version 3.0 or later see
+#                    https://www.gnu.org/licenses/gpl-3.0.txt
+# -----------------------------------------------------------------------------
+# BEGIN_PYTHON
+import numpy
+import copy
+def simple_inv(A) :
+	nr, nc = A.shape
+	assert nr == nc
+	#
+	# initialize Tablue = [A | I]
+	Tablue = numpy.empty( (nr, 2 * nr), dtype=type(A[0,0]) )
+	Tablue[:,0:nr] = A
+	for i in range(nr) :
+		for j in range(nr) :
+			Tablue[i,nr+j] = float(i == j)
+	# ------------------------------------------------------------------------
+	# Use row reduction to get convert A to an upper traingular matrix
+	# with ones along the diagonal
+	# ------------------------------------------------------------------------
+	# for each column (except the last) of the matrix
+	for j in range(nr - 1) :
+		# next row is one with maximum absolute element in column j
+		i_max = numpy.argmax( numpy.fabs( Tablue[:,j] ) )
+		#
+		# swap row j and row i_max
+		if i_max != j :
+			tmp              = copy.copy( Tablue[j,:] )
+			Tablue[j,:]      = Tablue[i_max,:]
+			Tablue[i_max,:]  = tmp
+		#
+		# divide row j by Tablue[j,j]
+		Tablue[j,:] = Tablue[j,:] / Tablue[j,j]
+		Tablue[j,j] = 1.0
+		#
+		# zero out column j in rows after j
+		for k in range(nr - j - 1) :
+			i = j + k + 1
+			Tablue[i,:] = Tablue[i,:] - Tablue[i,j] * Tablue[j,:]
+			Tablue[i,j]    = 0.0
+	# divide the last row of Tablue by the last pivot element
+	Tablue[nr-1,:]     = Tablue[nr-1,:] / Tablue[nr-1,nr-1]
+	Tablue[nr-1, nr-1] = 1.0
+	# ------------------------------------------------------------------------
+	# Use row reduction to get convert upper traingular matrix to the identity
+	# ------------------------------------------------------------------------
+	for jm in reversed(range(nr - 1) ) :
+		j = jm + 1
+		# zero out entries in column j and row less than j
+		for i in range(j) :
+			Tablue[i,:] = Tablue[i,:] - Tablue[i,j] * Tablue[j,:]
+			Tablue[i,j] = 0.0
+	# -------------------------------------------------------------------------
+	Ainv = Tablue[:, nr:]
+	return Ainv
+# END_PYTHON
+#
+# $begin numeric_simple_inv$$ $newlinech #$$
+# $spell
+#	inv
+#	Ainv
+# $$
+#
+# $section An AD Compatible Matrix Inverse Routine$$
+#
+# $head Syntax$$
+# $icode%Ainv% = simple_inv(%A%)%$$
+#
+# $head Purpose$$
+# This routine can be used with $code ad_double$$
+#
+# $head A$$
+# This must be an invertible square matrix (no singular detection is done).
+# The type of its elements can be $code float$$ or $code a_double$$.
+#
+# $head Ainv$$
+# This is the matrix inverse of $icode A$$.
+#
+# $children%
+#	example/python/numeric/simple_inv_xam.py
+# %$$
+# $head Example$$
+# $cref numeric_simple_inv_xam.py$$
+#
+# $head Source Code$$
+# When viewing the source code below it is important to know that
+# optimizes out multiplication by the constant one while recording a function.
+# It also optimizes out both addition and multiplication by the constant zero.
+# $srcthisfile%
+#	0%# BEGIN_PYTHON%# END_PYTHON%0
+# %$$
+#
+# $end
---
+++ example/python/numeric/simple_inv_xam.py
@@ -0,0 +1,81 @@
+# -----------------------------------------------------------------------------
+#         cppad_py: A C++ Object Library and Python Interface to Cppad
+#          Copyright (C) 2017-20 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#              GNU General Public License version 3.0 or later see
+#                    https://www.gnu.org/licenses/gpl-3.0.txt
+# -----------------------------------------------------------------------------
+# $begin numeric_simple_inv_xam.py$$ $newlinech #$$
+#
+# $section Example Computing Derivatives of Matrix Inversion$$
+#
+# $head Problem$$
+# We define
+# $latex \[
+#	A(x) = \left( \begin{array}{cc}
+#	x_0 & x_1 \\
+#	x_2 & x_3
+#	\end{array} \right)
+# \] $$
+# It follows that
+# $latex \[
+#	A^{-1}(x) =
+#	\frac{1}{x_3 * x_0 - x_1 * x2}
+#	\left( \begin{array}{cc}
+#	x_3 & - x_1 \\
+#	- x_2 & x_0
+#	\end{array} \right)
+# \] $$
+# We define
+# $latex \[
+#	f(x) = (x_3 * x_0 - x_1 * x2) [
+#	A_{0,0}^{-1} (x),
+#	A_{0,1}^{-1} (x),
+#	A_{1,0}^{-1} (x),
+#	A_{1,1}^{-1} (x)
+#	]
+# = [ x_3, -x_1, -x_2, x_0]
+# \] $$
+# The following example below check the derivative of $latex f(x)$$
+#
+# $head Source Code$$
+# $srcthisfile%
+#	0%# BEGIN_PYTHON%# END_PYTHON%1
+# %$$
+# $end
+#
+# BEGIN_PYTHON
+import numpy
+import cppad_py
+from simple_inv import simple_inv
+def simple_inv_xam() :
+	ok = True
+	x  = numpy.array( [1.0, 2.0, 3.0, 4.0] )
+	ax = cppad_py.independent(x)
+	#
+	# create the matrix [ [x_0, x_1], [x_2, x_3] ]
+	aA = numpy.reshape(ax, (2,2), order='C' )
+	#
+	# compute inverse of A
+	aAinv = simple_inv(aA)
+	#
+	# create f(x)
+	ay = numpy.reshape(aAinv, 4, order='C' )
+	ay = (ax[3] * ax[0] - ax[1] * ax[2]) * ay
+	f  = cppad_py.d_fun(ax, ay)
+	#
+	# evaluate the derivative f'(x)
+	J      = f.jacobian(x)
+	#
+	# Check the derivative
+	check      = numpy.zeros( (4, 4) )
+	check[0,3] =  1.0
+	check[1,1] = -1.0
+	check[2,2] = -1.0
+	check[3,0] =  1.0
+	#
+	eps99 = 99.0 * numpy.finfo(float).eps
+	ok   &= numpy.all( numpy.fabs(J - check) < eps99 )
+	return ok
+
+# END_PYTHON
