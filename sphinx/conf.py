9debb82a236887a7ae2811a612dae9b89d342ae8
Brad Bell <bradbell@seanet.com>
2020-07-30 06:19:15 -0700

    master: Advance to cppad_py-2020.7.30

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -23,7 +23,7 @@ import sphinx_rtd_theme
 project   = 'xsrst'
 copyright = '2020'
 author    = 'Brad Bell'
-version = '2020.7.29'
+version = '2020.7.30'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
