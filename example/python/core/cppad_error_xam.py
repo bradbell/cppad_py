77034ed77b5ddb0c776ede7e7925404e897f81dd
Brad Bell <bradbell@seanet.com>
2020-11-20 22:19:08 -0700

    master: add check_for_nan, pow_int, and CppAD exception mapping.

---
+++ example/python/core/cppad_error_xam.py
@@ -0,0 +1,70 @@
+# -----------------------------------------------------------------------------
+#         cppad_py: A C++ Object Library and Python Interface to Cppad
+#          Copyright (C) 2017-20 Bradley M. Bell (bradbell@seanet.com)
+#              This program is distributed under the terms of the
+#              GNU General Public License version 3.0 or later see
+#                    https://www.gnu.org/licenses/gpl-3.0.txt
+# -----------------------------------------------------------------------------
+# cppad_error
+# -----------------------------------------------------------------------------
+# BEGIN SOURCE
+def cppad_error_xam() :
+    #
+    import numpy
+    import cppad_py
+    #
+    # initialize return variable
+    ok = True
+    # ---------------------------------------------------------------------
+    n_ind = 1 # number of independent variables
+    n_dep = 2 # number of dependent variables
+    #
+    # dimension some vectors
+    x  = numpy.empty(n_ind, dtype=float)
+    ay = numpy.empty(n_dep, dtype=cppad_py.a_double)
+    #
+    # independent variables
+    x[0]  = 0.0
+    ax    = cppad_py.independent(x)
+    #
+    # dependent variables
+    ay[0] = ax[0] ** 2.0
+    ay[1] = cppad_py.pow_int(ax[0], 2)
+    #
+    # define f(x) = y
+    f = cppad_py.d_fun(ax, ay)
+    #
+    # Attempt to use first order before zero order
+    try :
+        y   = f.forward(1, x)
+    except RuntimeError as error :
+        message = str(error)
+        index   = 0
+        index   = message.find('file =', index)
+        ok      = ok and 0 < index
+        index   = message.find('line =', index)
+        ok      = ok and 0 < index
+        index   = message.find('exp =', index)
+        ok      = ok and 0 < index
+        index   = message.find('msg =', index)
+        ok      = ok and 0 < index
+    #
+    return ok
+#
+# END SOURCE
+# -----------------------------------------------------------------------------
+# {xsrst_comment_ch #}
+#
+# {xsrst_begin cppad_error_xam}
+#
+# .. include:: ../preamble.rst
+#
+# {xsrst_spell
+# }
+# Python: Example CppAD Error Message
+# ###################################
+# {xsrst_file
+#   # BEGIN SOURCE
+#   # END SOURCE
+# }
+# {xsrst_end cppad_error_xam}
---
+++ lib/cplusplus/cppad_error.cpp
@@ -0,0 +1,91 @@
+/* -----------------------------------------------------------------------------
+           cppad_py: A C++ Object Library and Python Interface to Cppad
+            Copyright (C) 2017-20 Bradley M. Bell (bradbell@seanet.com)
+                This program is distributed under the terms of the
+                GNU General Public License version 3.0 or later see
+                      https://www.gnu.org/licenses/gpl-3.0.txt
+----------------------------------------------------------------------------- */
+# include <stdexcept>
+# include <cppad/utility/error_handler.hpp>
+# include <cppad/utility/to_string.hpp>
+
+/*
+{xsrst_begin cppad_error}
+{xsrst_spell
+    namespace
+    cppad
+    exp
+}
+
+Converting CppAD Errors To Python Exceptions
+############################################
+
+Code
+****
+The following CppAD error handler is created
+in the empty namespace corresponding to the ``cppad_error.cpp`` file:
+{xsrst_code cpp}
+    CppAD::ErrorHandler cppad_error(handler);
+{xsrst_code}
+
+handler
+*******
+The error handler includes the following information in the exception message:
+
+.. csv-table::
+    :header: Label, Description
+    :widths: 10, 90
+
+    *file* , The name of the CppAD file where the error occurred.
+    *line* , The line number in the file where the error occurred.
+    *exp*  , The c++ logical expression that should have been true.
+    *msg*  , A descriptive error message about the problem.
+
+C++ throw
+*********
+A ``std::runtime_error`` *cpp_err* is thrown with
+*cpp_err*\ ``.what()`` a string containing the information above.
+
+Python raise
+************
+The swig wrapper for each call to CppAD will catch
+the ``std::runtime_error`` and raise a python
+``RuntimeError`` *py_err* where ``str``\ ( *py_err* )
+is a string containing the information above.
+
+Example
+*******
+{xsrst_child_list
+    example/python/core/cppad_error_xam.py
+}
+
+
+{xsrst_end cppad_error}
+*/
+namespace { // BEGIN_EMPTY_NAMESPACE
+    void handler(
+        bool known       ,
+        int  line        ,
+        const char *file ,
+        const char *exp  ,
+        const char *msg  )
+     {  // use the most recent cppad_mixed fatal_error routine
+        std::string message;
+        //
+        message += "\nCppAD: file = ";
+        message += file;
+        //
+        message += "\nline = ";
+        message += CppAD::to_string(line);
+        //
+        message += "\nexp = ";
+        message += exp;
+        //
+        message += "\nmsg = ";
+        message += msg;
+        //
+        std::runtime_error e(message);
+        throw(e);
+    }
+    CppAD::ErrorHandler cppad_error(handler);
+} // END_EMPTY_NAMESPACE
