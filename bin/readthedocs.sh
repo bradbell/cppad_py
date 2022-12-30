b69c100f2aa048551d16c6b6396f16c8e2365474
Brad Bell <bradbell@seanet.com>
2022-12-30 09:03:30 -0700

    master: readthedocs.sh: move doc from readdocs branch to master branch.

--- bin/readthedocs.sh
+++ bin/readthedocs.sh
@@ -4,7 +4,6 @@
 # SPDX-FileContributor: 2017-22 Bradley M. Bell
 # ----------------------------------------------------------------------------
 run_branch='master'
-doc_branch='readthedocs'
 # -----------------------------------------------------------------------------
 # bash function that echos and executes a command
 echo_eval() {
@@ -32,18 +31,11 @@ then
    exit 1
 fi
 # -----------------------------------------------------------------------------
-if ! grep '^/rst/' .gitignore > /dev/null
-then
-   echo 'Cannot find following line in .gitignore'
-   echo '/rst/'
-   exit 1
-fi
-# -----------------------------------------------------------------------------
-file_list='.nojekyll .gitignore .readthedocs.yaml'
-if ! git show-ref $doc_branch > /dev/null
-then
+# .readthedocs.yaml
+# Changes to this file need to be checked in to be used by Read the Docs
 cat << EOF > .readthedocs.yaml
 version: 2
+
 build:
    os: "ubuntu-20.04"
    tools:
@@ -53,60 +45,44 @@ build:
          - pip install furo
       post_build:
          - cp rst/_sources/*.txt rst/_build/html/_sources
+
+sphinx:
+   configuration: rst/conf.py
 EOF
-cat << EOF
-readthedocs.sh: git cannot find the $doc_branch branch.
-Use the following commands to create it:
-
-   git checkout --orphan $doc_branch
-   git reset --hard
-
-   git show $run_branch:.gitignore > .gitignore
-   sed -i .gitignore -e 's|^/rst/$|# &|'
-
-   touch .nojekyll
-
-   git add $file_list
-   git commit -m 'create $doc_branch branch' $file_list
-   git checkout $run_branch
-EOF
-   exit 1
-fi
-if ! git show-ref --heads --quiet $doc_branch
-then
-   echo "Cannot find local copy of the following $doc_branch branch"
-   git show-ref | grep $doc_branch
-    echo 'Perhaps the following would create the local copy ?'
-    echo "  git checkout -b $doc_branch origin/$doc_branch"
-    echo "  git checkout $run_branch"
-   exit 1
-fi
 # -----------------------------------------------------------------------------
 # xsrst
-dir=./build/rst
-if [ -e $dir ]
-then
-   echo_eval rm -r $dir
-fi
-echo_eval xrst --page_source --rst_only
-if [ ! -e $dir ]
-then
-   echo "readthedocs.sh: xrst did not create the $dir directory"
-   exit 1
-fi
+dir_list='
+   ./build/rst
+   ./build/html
+'
+for dir in $dir_list
+do
+   if [ -e $dir ]
+   then
+      echo_eval rm -r $dir
+   fi
+done
+echo_eval xrst --page_source
+for dir in $dir_list
+do
+   if [ ! -e $dir ]
+   then
+      echo "readthedocs.sh: xrst did not create the $dir directory"
+      exit 1
+   fi
+done
 # -----------------------------------------------------------------------------
 # version
 version=$(version.sh get)
 # -----------------------------------------------------------------------------
-# doc_branch
-echo_eval git checkout $doc_branch
-#
 # ./rst
 if [ -e ./rst ]
 then
    echo_eval rm -r ./rst
 fi
+# note the build/rst/_sources was moved to build/html/_soruces
 echo_eval cp -r ./build/rst ./rst
+echo_eval cp -r ./build/html/_sources ./rst/_sources
 #
 # git delete
 delete_list=$(git status -s | grep '^ D ' | sed -e 's|^ D ||')
